515237fd614df4b7ad6388b19ca4f65a6c1df686
Bruno Couto Joao <bruno.couto.joao@gmail.com>
2025-03-20 17:10:38 -0300

    Adiciona índice de imagens para o frontend

---
+++ frontend/public/img/index.js
@@ -0,0 +1,36 @@
+/**
+ * Exporta todas as imagens para uso em componentes
+ */
+
+export default {
+  // Imagens principais
+  logo: '/img/logo.png',
+  logoWhite: '/img/logoWhite.png',
+  background: '/img/bg.jpg',
+  marker: '/img/marker.png',
+  circle: '/img/circle.png',
+  pointer: '/img/pointer.png',
+  car: '/img/car.png',
+  upload: '/img/upload.png',
+  download: '/img/download.png',
+  
+  // Imagens de veículos
+  cars: {
+    car: '/img/cars/car.png',
+    truck: '/img/cars/truck.png',
+    bus: '/img/cars/bus.png',
+    motorcycle: '/img/cars/motorcycle.png',
+    bicycle: '/img/cars/bicycle.png',
+    person: '/img/cars/person.png',
+    boat: '/img/cars/boat.png',
+    plane: '/img/cars/plane.png',
+    default: '/img/cars/default.png'
+  },
+  
+  // Status de ícones
+  status: {
+    online: '/img/css/online.svg',
+    offline: '/img/css/offline.svg',
+    unknown: '/img/css/unknown.svg'
+  }
+};
